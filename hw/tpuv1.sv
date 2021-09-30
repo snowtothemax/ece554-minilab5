0e9687e6371bfc794a86093dde51e89ec871561c
akseltorgerson <akseltorgerson@gmail.com>
2021-09-29 19:34:04 -0500

    updated bounds

--- hw/tpuv1.sv
+++ hw/tpuv1.sv
@@ -14,7 +14,7 @@ module tpuv1
    );
    
 	localparam ROWBITS = $clog2(DIM);
-	localparam COUNTER_BITS = $ceil($clog2(DIM*3 - 2));
+	localparam COUNTER_BITS = $clog2(DIM*3);
 	
 	///////////////////////////////
 	/// intermediate signals
